fb8171a010dbabca28cd7d20c9c916985b43d694
rejetisomeswararao <rejetisomeswararao@gmail.com>
2025-09-27 02:04:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function first() {
+  var x = window.prompt("ENTER YOUR NAME");
+ if(x===null||x.trim() === "") {
+    document.getElementById("welcome").innerHTML = "welcome to garena";
+  } else {
+    document.getElementById("welcome").innerHTML = x +" "+ "welcome to garena";
+  }
+}
+first();
+ function sub() {
+      open("https://rejetisomeswararao.github.io/FFMSG/")
+    }
